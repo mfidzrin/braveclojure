18775aae56d3387dac2374d0fc5cb1926e65b96c
Mohd Fidzrin Mohd Hamizaz <mohdfidzrin@gmail.com>
2022-10-08 14:26:04 +0800

    stop at the shire's next top model body parts

--- ch3/ch3.clj
+++ ch3/ch3.clj
@@ -396,3 +396,55 @@ failed-protagonist-names
 ;;  function body)
 (map (fn [name] (str "Hi, " name))
      ["Darth Vader" "Aloha "])
+
+(def my-special-multiplier (fn [x] (* x 3)))
+
+(my-special-multiplier 12)
+
+(#(* % 3) 8)
+
+(map #(str "Hi, " %)
+     ["Darth Vader" "Mr. Magoo"])
+
+;; Distinction between function call and anonymous function
+;; function call
+(* 8 3)
+
+;; Anonymous function
+#(* % 3)
+
+;; use the anonymous function
+(#(* % 3) 8)
+
+;; the percent sign %, indicates the argument passed to the function
+;; for multiple arguments, %1 %2 %3
+
+(#(str %1 " and " %2) "cornbread" "butter beans")
+
+;; we can also pass a rest parameters with %&
+(#(identity %&) 1 "blarg" :yip)
+
+;; Pulling It All Together
+;; The Shire's Next Top Model
+;; For our hobbit model, we'll eschew such hobbit characteristics as joviality and
+;; mischievousness and focus only on the hobbit's tiny body. Here's the hobbit model:
+
+(def asym-hobbit-body-parts [{:name "head"           :size 3}
+                             {:name "left-eye"       :size 1}
+                             {:name "left-ear"       :size 1}
+                             {:name "mouth"          :size 1}
+                             {:name "nose"           :size 1}
+                             {:name "neck"           :size 2}
+                             {:name "left-shoulder"  :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest"          :size 10}
+                             {:name "back"           :size 10}
+                             {:name "left-forearm"   :size 3}
+                             {:name "abdomen"        :size 6}
+                             {:name "left-kidney"    :size 1}
+                             {:name "left-hand"      :size 2}
+                             {:name "left-knee"      :size 2}
+                             {:name "left-thigh"     :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles"  :size 1}
+                             {:name "left-foot"      :size 2}])
